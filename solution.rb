5e1ceb1b3e0eb17e42670db9e9614f9724d05d59
prmoreira23 <prm.gredes@gmail.com>
2018-05-21 12:22:21 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,21 +1,22 @@
-def palindrome(string)
+def palindrome?(string)
   string.downcase!
   string == string.reverse
 end
 
-puts "Pablo #{palindrome("pablo")}"
-puts "Racecar #{palindrome("racecar")}"
-puts "Noon #{palindrome("noon")}"
+puts "Pablo #{palindrome?("pablo")}"
+puts "Racecar #{palindrome?("racecar")}"
+puts "Noon #{palindrome?("noon")}"
+puts "Ili #{palindrome?("Ili")}"
 
-def palindrome_recursive(string)
-  return true if string.size == 1 || string.size == 0
+def palindrome_recursive?(string)
+  return true if string.size < 2
   if(string.size >= 2)
-    if string[0] == string[1]
-      return palindrome_recursive(string.slice(1, string.size - 2))
+    if string[0] == string[-1]
+      return true if palindrome_recursive?(string.slice(1, string.size - 2))
     else
       return false
     end
   end
 end
 
-puts palindrome_recursive("ili")
+puts palindrome_recursive?("racecara")
