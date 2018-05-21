0a9bd4674e4f61e757d5a12807dfa5d79fcad225
AyyLam <al3101@nyu.edu>
2018-05-21 12:07:33 -0400

    Completed

---
+++ solution.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+def palindrome?(phrase)
+  phrase.reversed == phrase
+end
+
+def palindrome2?(phrase)
+  if phrase.length == 0 || phrase.length == 1
+    return true
+  elsif phrase[0] == phrase[-1]
+    phrase = phrase[1..phrase.length-2]
+    return palindrome2?(phrase)
+  else
+    return false
+  end
+end
